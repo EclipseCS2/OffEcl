bdbb9ac2d9c86e75698010c312c9e0ba9b0ad5d2
Eclipse <invayt01@gmail.com>
2026-07-21 14:49:17 +0300

    Add files via upload

--- output/client_dll.hpp
+++ output/client_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-17 17:24:29.481698200 UTC
+// 2026-07-21 10:34:25.508080800 UTC
 
 #pragma once
 
@@ -10,7 +10,7 @@ namespace cs2_dumper {
     namespace schemas {
         // Module: client.dll
         // Class count: 563
-        // Enum count: 19
+        // Enum count: 14
         namespace client_dll {
             // Alignment: 4
             // Member count: 5
--- output/steamaudio_dll.cs
+++ output/steamaudio_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-17 17:24:29.481698200 UTC
+// 2026-07-21 10:34:25.508080800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: steamaudio.dll
